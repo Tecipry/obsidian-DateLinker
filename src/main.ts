e51f36d630efadb5a022a564571b2e1c92616ed3
Tecipry <roald.gs@gmail.com>
2026-09-22 22:25:31 +0200

    first rough functionality

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
-import {
-	Editor,
-	MarkdownView,
-	MarkdownFileInfo,
-	Modal,
-	Notice,
-	Plugin,
-} from 'obsidian';
+import { Notice, Plugin, TFile } from 'obsidian';
 import {
 	DEFAULT_SETTINGS,
 	MyPluginSettings,
 	SampleSettingTab,
 } from './settings';
-
-// Remember to rename these classes and interfaces!
 
 export default class MyPlugin extends Plugin {
 	settings!: MyPluginSettings;
@@ -20,73 +11,96 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (
-				editor: Editor,
-				_ctx: MarkdownView | MarkdownFileInfo,
-			) => {
-				editor.replaceSelection('Sample editor command');
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			},
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(activeDocument, 'click', (_evt: MouseEvent) => {
-			new Notice('Click');
-		});
+		// this.registerDomEvent(activeDocument, 'click', (_evt: MouseEvent) => {
+		// 	new Notice('Click');
+		// });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000),
-		);
+		this.addCommand({
+			id: 'update-managed-relations-all-files',
+			name: 'Update managed relations for all files',
+			callback: () => {
+				this.processFrontmatterForAllFiles();
+			},
+		});
+		this.addCommand({
+			id: 'update-managed-relations-this-file',
+			name: 'Update managed relations for this file',
+			callback: () => {
+				const activeFile = this.app.workspace.getActiveFile();
+				// activeFile might be null
+				if (!activeFile) {
+					return;
+				}
+				this.processFrontmatterForFile(activeFile);
+				new Notice(`Processed note.`);
+			},
+		});
 	}
 
 	onunload() {}
+
+	async processFrontmatterForAllFiles(): Promise<void> {
+		const files: TFile[] = this.app.vault.getMarkdownFiles();
+		let modifiedCount = 0;
+
+		for (const file of files) {
+			this.processFrontmatterForFile(file);
+			modifiedCount++;
+		}
+
+		new Notice(`Processed ${modifiedCount} note(s).`);
+	}
+
+	async processFrontmatterForFile(file: TFile): Promise<void> {
+		const cache = this.app.metadataCache.getFileCache(file);
+		const frontmatter = cache?.frontmatter;
+
+		if (
+			frontmatter &&
+			Object.prototype.hasOwnProperty.call(
+				frontmatter,
+				'linkProbertyDatesToDailyNote',
+			)
+		) {
+			const propertysToCheckForDates: Array<string> =
+				frontmatter['linkProbertyDatesToDailyNote'];
+			let managedRelations = [];
+
+			// extract dates
+			for (const property of propertysToCheckForDates) {
+				if (
+					!Object.prototype.hasOwnProperty.call(frontmatter, property)
+				) {
+					// specified property is not in frontmatter
+					continue;
+				}
+
+				const dateRaw = frontmatter[property];
+				const parsedDate = window.moment(
+					dateRaw,
+					'YYYY-MM-DDTHH:mm:ssZ',
+				); // importing moment from 'obsidian' doesn't work atm
+				if (!parsedDate.isValid()) {
+					continue;
+				}
+
+				const relation = {
+					property: property,
+					link: `[[${parsedDate.format('DD.MM.YYYY')}]]`,
+				};
+				managedRelations.push(relation);
+			}
+
+			this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+				frontmatter['managed-relations'] = managedRelations;
+			});
+		}
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
@@ -98,17 +112,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
